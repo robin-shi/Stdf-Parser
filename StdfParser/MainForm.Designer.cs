d6310deea2becf53ab0799665c873e65b16734ee
Robin <robin_shi@spirox.com>
2022-08-20 00:33:25 +0800

    Some modifications

--- StdfParser/MainForm.Designer.cs
+++ StdfParser/MainForm.Designer.cs
@@ -29,20 +29,94 @@ namespace StdfParser
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridViewTestItems = new System.Windows.Forms.DataGridView();
+            this.TestNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTestItems)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(926, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(39, 21);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenStdfFileDialog);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // dataGridViewTestItems
+            // 
+            this.dataGridViewTestItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTestItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TestNum});
+            this.dataGridViewTestItems.Location = new System.Drawing.Point(12, 48);
+            this.dataGridViewTestItems.Name = "dataGridViewTestItems";
+            this.dataGridViewTestItems.ReadOnly = true;
+            this.dataGridViewTestItems.RowTemplate.Height = 23;
+            this.dataGridViewTestItems.Size = new System.Drawing.Size(230, 460);
+            this.dataGridViewTestItems.TabIndex = 1;
+            // 
+            // TestNum
+            // 
+            this.TestNum.Frozen = true;
+            this.TestNum.HeaderText = "TestName";
+            this.TestNum.Name = "TestNum";
+            this.TestNum.ReadOnly = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(926, 532);
+            this.Controls.Add(this.dataGridViewTestItems);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "StdfParser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTestItems)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridViewTestItems;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TestNum;
     }
 }
 
--- StdfParser/MainForm.cs
+++ StdfParser/MainForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LinqToStdf;
+using LinqToStdf.Records.V4;
 
 namespace StdfParser
 {
@@ -15,6 +17,39 @@ namespace StdfParser
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        StdfFile stdfFile { get; set; }
+        List<string> TestNames { get; set; }
+        private void OpenStdfFileDialog(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog() {Filter="stdf|*.stdf;*.std",
+            InitialDirectory=@"D:\"};
+            fileDialog.ShowDialog();
+            try
+            {
+                stdfFile = new StdfFile(fileDialog.FileName);
+                UpdateTestItems();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void UpdateTestItems()
+        {
+            var results = stdfFile.GetRecords().OfExactType<Tsr>().Select(p=>p.TestName);
+            foreach (var result in results)
+            {
+                //TestNames.Add(result);
+                dataGridViewTestItems.Rows.Add(result);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
         }
     }
 }
